6056128b206cf2f88f5d22f6f0e4cf98c6076aea
Tin Tvrtkovic <tinchester@gmail.com>
2016-09-06 01:03:16 +0200

    Event loop policy now set by a fixture setup hook wrapper.

--- tests/test_dependent_fixtures.py
+++ tests/test_dependent_fixtures.py
@@ -7,3 +7,22 @@ import pytest
 def test_dependent_fixture(dependent_fixture):
     """Test a dependent fixture."""
     yield from asyncio.sleep(0.1)
+
+
+@pytest.fixture
+def assert_no_global_loop(event_loop):
+    """A dummy fixture, to assert the event loop policy has been set."""
+    assert event_loop
+
+    with pytest.raises(NotImplementedError):
+        asyncio.get_event_loop()
+    yield
+    with pytest.raises(NotImplementedError):
+        asyncio.get_event_loop()
+
+
+@pytest.mark.asyncio(forbid_global_loop=True)
+@asyncio.coroutine
+def test_fixture_no_global_loop(assert_no_global_loop):
+    """The fixture will assert the correct policy has been set."""
+    pass
