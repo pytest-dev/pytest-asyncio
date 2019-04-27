b57f82b885db3c21034c256e29a2d5d48c3b698a
Roman Tolkachyov <roman@tolkachyov.name>
2019-04-27 10:27:40 +0300

    Shutdown async generators before closing a loop

--- pytest_asyncio/plugin.py
+++ pytest_asyncio/plugin.py
@@ -206,6 +206,7 @@ def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
+    loop.run_until_complete(loop.shutdown_asyncgens())
     loop.close()
 
 
