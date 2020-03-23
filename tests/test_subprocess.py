d5a0f4789e7fecb58d509409e2c537b206c4fde2
simonfagerholm <zlim00@gmail.com>
2020-03-23 10:59:06 +0100

    Enable test_subprocess to be run on win, by changing to ProactorEventLoop in those tests.

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -6,6 +6,17 @@ import asyncio.subprocess
 import pytest
 
 
+if sys.platform == 'win32':
+    # The default asyncio event loop implementation on Windows does not
+    # support subprocesses. Subprocesses are available for Windows if a
+    # ProactorEventLoop is used.
+    @pytest.yield_fixture()
+    def event_loop():
+        loop = asyncio.ProactorEventLoop()
+        yield loop
+        loop.close()
+
+
 @pytest.mark.asyncio(forbid_global_loop=False)
 async def test_subprocess(event_loop):
     """Starting a subprocess should be possible."""
