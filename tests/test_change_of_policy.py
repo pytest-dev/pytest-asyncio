84e63f1e1dd9abd94bcb97f85109b3257edc4ee0
alblasco <angelluisblasco@live.com>
2020-06-28 11:56:18 +0200

    To simplify the hew test case (test_1 is not needed)

--- tests/test_change_of_policy.py
+++ tests/test_change_of_policy.py
@@ -8,15 +8,13 @@ def old_policy():
     yield policy
 
 
-def test_1(old_policy):
+
+@pytest.mark.asyncio
+async def test_1(old_policy):
     assert old_policy == asyncio.get_event_loop_policy()
 
 @pytest.mark.asyncio
 async def test_2(old_policy):
-    assert old_policy == asyncio.get_event_loop_policy()
-
-@pytest.mark.asyncio
-async def test_3(old_policy):
     # This test fails with pytest-asyncio V0.14
     # due to a teardown change of event loop policy
     assert old_policy == asyncio.get_event_loop_policy()
