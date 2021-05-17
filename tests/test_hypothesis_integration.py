328e2624f33680abc51632c326b58cb82650906a
Michael Seifert <m.seifert@digitalernachschub.de>
2021-05-17 16:08:54 +0200

    Adjusted Hypothesis integration test to use the same event loop initialization as in the plugin.
    The plugin code creates a new event loop from the current event loop policy, whereas the fixture in the Hypothesis test uses get_event_loop. In Python 3.10 the use of get_event_loop was deprecated and causes the Hypothesis tests to fail.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/test_hypothesis_integration.py
+++ tests/test_hypothesis_integration.py
@@ -10,8 +10,9 @@ from hypothesis import given, strategies as st
 
 @pytest.fixture(scope="module")
 def event_loop():
-    loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
+    loop.close()
 
 
 @given(st.integers())
