452d7af5a7bcf5e523be2fbfdfb2fdfb6a35b7a4
Michael Seifert <m.seifert@digitalernachschub.de>
2021-05-21 08:15:03 +0200

    refactor: test_async_fixtures_with_finalizer no longer trigger a DeprecationWarning on Python 3.10.
    The code was adjusted to use asyncio.get_event_loop_policy().get_event_loop() rather than the deprecated asyncio.get_event_loop().
    Some additional comments were added to clarify the circumstances under which the test can fail.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/async_fixtures/test_async_fixtures_with_finalizer.py
+++ tests/async_fixtures/test_async_fixtures_with_finalizer.py
@@ -44,11 +44,14 @@ async def port_with_event_loop_finalizer(request, event_loop):
 async def port_with_get_event_loop_finalizer(request, event_loop):
     def port_finalizer(finalizer):
         async def port_afinalizer():
-            # await task using loop provided by asyncio.get_event_loop()
-            # RuntimeError is raised if task is created on a different loop
+            # await task using current loop retrieved from the event loop policy
+            # RuntimeError is raised if task is created on a different loop.
+            # This can happen when pytest_fixture_setup does not set up the loop correctly,
+            # for example when policy.set_event_loop() is called with a wrong argument
             await finalizer
 
-        asyncio.get_event_loop().run_until_complete(port_afinalizer())
+        current_loop = asyncio.get_event_loop_policy().get_event_loop()
+        current_loop.run_until_complete(port_afinalizer())
 
     worker = asyncio.ensure_future(asyncio.sleep(0.2))
     request.addfinalizer(functools.partial(port_finalizer, worker))
