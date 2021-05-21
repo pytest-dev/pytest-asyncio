6ec76477061ea14394cadbf2cef673b04971ef4d
Michael Seifert <m.seifert@digitalernachschub.de>
2021-05-21 08:21:13 +0200

    feat: Add support for Python 3.10.
    Adjusted changelog and setup.py accordingly.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Testing",
         "Framework :: Pytest",
     ],
