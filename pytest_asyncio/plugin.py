bec4750f143eb13177071a705ffeba311a6f0baa
Michael Seifert <m.seifert@digitalernachschub.de>
2022-01-05 11:18:45 +0100

    refactor: Removed use of obsolete transfer_markers during test collection phase.
    transfer_markers was removed in Pytest 4.1. The minimum required pytest version for pytest-asyncio is currently v5.4.0. That means that `transfer_markers` is always a no-op.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- pytest_asyncio/plugin.py
+++ pytest_asyncio/plugin.py
@@ -7,15 +7,6 @@ import socket
 
 import pytest
 
-try:
-    from _pytest.python import transfer_markers
-except ImportError:  # Pytest 4.1.0 removes the transfer_marker api (#104)
-
-    def transfer_markers(*args, **kwargs):  # noqa
-        """Noop when over pytest 4.1.0"""
-        pass
-
-
 from inspect import isasyncgenfunction
 
 
@@ -39,13 +30,6 @@ def pytest_pycollect_makeitem(collector, name, obj):
     """A pytest hook to collect asyncio coroutines."""
     if collector.funcnamefilter(name) and _is_coroutine(obj):
         item = pytest.Function.from_parent(collector, name=name)
-
-        # Due to how pytest test collection works, module-level pytestmarks
-        # are applied after the collection step. Since this is the collection
-        # step, we look ourselves.
-        transfer_markers(obj, item.cls, item.module)
-        item = pytest.Function.from_parent(collector, name=name)  # To reload keywords.
-
         if "asyncio" in item.keywords:
             return list(collector._genfunctions(name, obj))
 
