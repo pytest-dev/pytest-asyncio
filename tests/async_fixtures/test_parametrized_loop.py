3678a7ff9300c20259512336d5a8b1b6f761e86b
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-25 10:38:11 +0200

    Support parametrized event_loop fixture

---
+++ tests/async_fixtures/test_parametrized_loop.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import pytest
+
+TESTS_COUNT = 0
+
+
+def teardown_module():
+    assert TESTS_COUNT == 4
+
+
+@pytest.fixture(scope="module", params=[1, 2])
+def event_loop(request):
+    request.param
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(params=["a", "b"])
+async def fix(request):
+    await asyncio.sleep(0)
+    return request.param
+
+
+@pytest.mark.asyncio
+async def test_parametrized_loop(fix):
+    await asyncio.sleep(0)
+    global TESTS_COUNT
+    TESTS_COUNT += 1
