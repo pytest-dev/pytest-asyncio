aad03163c1d5c3c07b88300382c402e790d2fdce
Michael Seifert <m.seifert@digitalernachschub.de>
2022-03-10 08:55:36 +0100

    test: Package-scoped event_loop fixture no longer leaks into other tests.
    The expected behaviour is that the `event_loop` fixture defined in `tests/sessionloop/conftest.py` is torn down when all tests in `tests/sessionloop` are complete. Running the tests with the pytest option --setup-show pointed out that the fixture is torn down at the end of the test session, instead. This is an unintended side effect of the sessionloop test which may affect other tests in the test suite.
    
    Reducing the fixture scope from "package" to "module" results in the expected behaviour. The module was renamed to reflect the fact that the tests do not use a session scope.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/loop_fixture_scope/conftest.py
+++ tests/loop_fixture_scope/conftest.py
@@ -3,14 +3,14 @@ import asyncio
 import pytest
 
 
-class CustomSelectorLoopSession(asyncio.SelectorEventLoop):
+class CustomSelectorLoop(asyncio.SelectorEventLoop):
     """A subclass with no overrides, just to test for presence."""
 
 
-loop = CustomSelectorLoopSession()
+loop = CustomSelectorLoop()
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="module")
 def event_loop():
     """Create an instance of the default event loop for each test case."""
     yield loop
