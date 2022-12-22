99cfd71e40e97ae51771ae66bd18612234bd8eff
Michael Seifert <m.seifert@digitalernachschub.de>
2022-12-22 16:58:15 +0100

    Remove use of deprecated ::set-output GitHub command (#466)
    * [ci] Remove use of deprecated ::set-output GitHub command.
    
    The ::set-output command has been deprecated in favor of redirecting the output to a file. [0]
    
    This patch adjusts get-version.py to the new output syntax and silences tox output to avoid writing excess information to the file.
    
    [0] https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
    
    Closes #429
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>
    
    * [ci] Invoke get-version script directly, instead of starting it in a tox environment.
    
    This requires pytest-asyncio being installed.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>
    
    * [refactor] Removed obsolete "version-info" tox environment.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tools/get-version.py
+++ tools/get-version.py
@@ -8,9 +8,9 @@ from packaging.version import parse as parse_version
 def main():
     version_string = metadata.version("pytest-asyncio")
     version = parse_version(version_string)
-    print(f"::set-output name=version::{version}")
+    print(f"version={version}")
     prerelease = json.dumps(version.is_prerelease)
-    print(f"::set-output name=prerelease::{prerelease}")
+    print(f"prerelease={prerelease}")
 
 
 if __name__ == "__main__":
