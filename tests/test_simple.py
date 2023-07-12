39a97548bfb0fe0de9527a98563037cb1ce80899
Michael Seifert <m.seifert@digitalernachschub.de>
2023-07-12 11:00:37 +0200

    [test] The test that asserts compatibility with pytest.mark.skip reports success rather than "skipped" when the test succeeds.
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -265,9 +265,22 @@ class TestEventLoopStartedBeforeFixtures:
         assert await loop.run_in_executor(None, self.foo) == 1
 
 
-@pytest.mark.asyncio
-async def test_no_warning_on_skip():
-    pytest.skip("Test a skip error inside asyncio")
+def test_asyncio_marker_compatibility_with_skip(pytester: Pytester):
+    pytester.makepyfile(
+        dedent(
+            """\
+                import pytest
+
+                pytest_plugins = "pytest_asyncio"
+
+                @pytest.mark.asyncio
+                async def test_no_warning_on_skip():
+                    pytest.skip("Test a skip error inside asyncio")
+            """
+        )
+    )
+    result = pytester.runpytest("--asyncio-mode=strict")
+    result.assert_outcomes(skipped=1)
 
 
 def test_async_close_loop(event_loop):
