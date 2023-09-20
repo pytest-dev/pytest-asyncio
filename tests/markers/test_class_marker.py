1ac176675f462a7a678f0a190ff541b417857075
Michael Seifert <m.seifert@digitalernachschub.de>
2023-09-20 15:57:28 +0200

    [feat] Introduce the asyncio_event_loop mark which provides a class-scoped asyncio event loop when a class has the mark.
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/markers/test_class_marker.py
+++ tests/markers/test_class_marker.py
@@ -1,5 +1,6 @@
 """Test if pytestmark works when defined on a class."""
 import asyncio
+from textwrap import dedent
 
 import pytest
 
@@ -23,3 +24,83 @@ class TestPyTestMark:
 @pytest.fixture
 def sample_fixture():
     return None
+
+
+def test_asyncio_event_loop_mark_provides_class_scoped_loop_strict_mode(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.mark.asyncio_event_loop
+            class TestClassScopedLoop:
+                loop: asyncio.AbstractEventLoop
+
+                @pytest.mark.asyncio
+                async def test_remember_loop(self):
+                    TestClassScopedLoop.loop = asyncio.get_running_loop()
+
+                @pytest.mark.asyncio
+                async def test_this_runs_in_same_loop(self):
+                    assert asyncio.get_running_loop() is TestClassScopedLoop.loop
+            """
+        )
+    )
+    result = pytester.runpytest("--asyncio-mode=strict")
+    result.assert_outcomes(passed=2)
+
+
+def test_asyncio_event_loop_mark_provides_class_scoped_loop_auto_mode(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.mark.asyncio_event_loop
+            class TestClassScopedLoop:
+                loop: asyncio.AbstractEventLoop
+
+                async def test_remember_loop(self):
+                    TestClassScopedLoop.loop = asyncio.get_running_loop()
+
+                async def test_this_runs_in_same_loop(self):
+                    assert asyncio.get_running_loop() is TestClassScopedLoop.loop
+            """
+        )
+    )
+    result = pytester.runpytest("--asyncio-mode=auto")
+    result.assert_outcomes(passed=2)
+
+
+def test_asyncio_event_loop_mark_is_inherited_to_subclasses(pytester: pytest.Pytester):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.mark.asyncio_event_loop
+            class TestSuperClassWithMark:
+                pass
+
+            class TestWithoutMark(TestSuperClassWithMark):
+                loop: asyncio.AbstractEventLoop
+
+                @pytest.mark.asyncio
+                async def test_remember_loop(self):
+                    TestWithoutMark.loop = asyncio.get_running_loop()
+
+                @pytest.mark.asyncio
+                async def test_this_runs_in_same_loop(self):
+                    assert asyncio.get_running_loop() is TestWithoutMark.loop
+            """
+        )
+    )
+    result = pytester.runpytest("--asyncio-mode=strict")
+    result.assert_outcomes(passed=2)
