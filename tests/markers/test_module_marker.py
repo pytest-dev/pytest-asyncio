158f51776d1e7b8198ac9c53288b46d14fb243ca
Michael Seifert <m.seifert@digitalernachschub.de>
2023-09-26 15:03:09 +0200

    [refactor] Existing tests in test_module_marker are executed with pytest.Pytester to avoid applying pytestmark to subsequent tests in the test module.
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/markers/test_module_marker.py
+++ tests/markers/test_module_marker.py
@@ -1,39 +1,52 @@
-"""Test if pytestmark works when defined in a module."""
-import asyncio
+from textwrap import dedent
 
-import pytest
-
-pytestmark = pytest.mark.asyncio
+from pytest import Pytester
 
 
-class TestPyTestMark:
-    async def test_is_asyncio(self, event_loop, sample_fixture):
-        assert asyncio.get_event_loop()
+def test_asyncio_mark_works_on_module_level(pytester: Pytester):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
 
-        counter = 1
+            import pytest
 
-        async def inc():
-            nonlocal counter
-            counter += 1
-            await asyncio.sleep(0)
-
-        await asyncio.ensure_future(inc())
-        assert counter == 2
+            pytestmark = pytest.mark.asyncio
 
 
-async def test_is_asyncio(event_loop, sample_fixture):
-    assert asyncio.get_event_loop()
-    counter = 1
+            class TestPyTestMark:
+                async def test_is_asyncio(self, event_loop, sample_fixture):
+                    assert asyncio.get_event_loop()
 
-    async def inc():
-        nonlocal counter
-        counter += 1
-        await asyncio.sleep(0)
+                    counter = 1
 
-    await asyncio.ensure_future(inc())
-    assert counter == 2
+                    async def inc():
+                        nonlocal counter
+                        counter += 1
+                        await asyncio.sleep(0)
+
+                    await asyncio.ensure_future(inc())
+                    assert counter == 2
 
 
-@pytest.fixture
-def sample_fixture():
-    return None
+            async def test_is_asyncio(event_loop, sample_fixture):
+                assert asyncio.get_event_loop()
+                counter = 1
+
+                async def inc():
+                    nonlocal counter
+                    counter += 1
+                    await asyncio.sleep(0)
+
+                await asyncio.ensure_future(inc())
+                assert counter == 2
+
+
+            @pytest.fixture
+            def sample_fixture():
+                return None
+            """
+        )
+    )
+    result = pytester.runpytest("--asyncio-mode=strict")
+    result.assert_outcomes(passed=2)
