c8a52a018f09b43a9a228043959a8eebb3efff68
Michael Seifert <m.seifert@digitalernachschub.de>
2023-10-09 12:33:43 +0200

    [refactor] Tests for "loop_fixture_scope" create the custom event loop inside the event_loop fixture override rather than on the module-level.
    This prevents the custom loop from being created during test collection time.
    
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/loop_fixture_scope/conftest.py
+++ tests/loop_fixture_scope/conftest.py
@@ -7,11 +7,9 @@ class CustomSelectorLoop(asyncio.SelectorEventLoop):
     """A subclass with no overrides, just to test for presence."""
 
 
-loop = CustomSelectorLoop()
-
-
 @pytest.fixture(scope="module")
 def event_loop():
     """Create an instance of the default event loop for each test case."""
+    loop = CustomSelectorLoop()
     yield loop
     loop.close()
