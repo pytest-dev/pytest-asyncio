a7b1c39be72e976547618a4e543123aafce1f73b
Michael Seifert <m.seifert@digitalernachschub.de>
2023-11-05 14:55:03 +0100

    [feat!] Replaced the asyncio_event_loop marker with an optional "scope" kwarg to the asyncio mark.
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- docs/source/reference/markers/class_scoped_loop_strict_mode_example.py
+++ docs/source/reference/markers/class_scoped_loop_strict_mode_example.py
@@ -3,14 +3,12 @@ import asyncio
 import pytest
 
 
-@pytest.mark.asyncio_event_loop
+@pytest.mark.asyncio(scope="class")
 class TestClassScopedLoop:
     loop: asyncio.AbstractEventLoop
 
-    @pytest.mark.asyncio
     async def test_remember_loop(self):
         TestClassScopedLoop.loop = asyncio.get_running_loop()
 
-    @pytest.mark.asyncio
     async def test_this_runs_in_same_loop(self):
         assert asyncio.get_running_loop() is TestClassScopedLoop.loop
