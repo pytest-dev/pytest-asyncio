dd129e9c4ce92cc8af50b82551e23ba2d857f8da
Michael Seifert <m.seifert@digitalernachschub.de>
2024-01-28 19:44:04 +0100

    [fix] Fixes a bug that prevented tests from being collected from package when any module inside the package used a pytest.skip statement.
    Signed-off-by: Michael Seifert <m.seifert@digitalernachschub.de>

--- tests/test_skips.py
+++ tests/test_skips.py
@@ -105,3 +105,33 @@ def test_unittest_skiptest_compatibility(pytester: Pytester):
     )
     result = pytester.runpytest("--asyncio-mode=auto")
     result.assert_outcomes(skipped=1)
+
+
+def test_skip_in_module_does_not_skip_package(pytester: Pytester):
+    pytester.makepyfile(
+        __init__="",
+        test_skip=dedent(
+            """\
+                import pytest
+
+                pytest.skip("Skip all tests", allow_module_level=True)
+
+                def test_a():
+                    pass
+
+                def test_b():
+                    pass
+            """
+        ),
+        test_something=dedent(
+            """\
+                import pytest
+
+                @pytest.mark.asyncio
+                async def test_something():
+                    pass
+            """
+        ),
+    )
+    result = pytester.runpytest("--asyncio-mode=strict")
+    result.assert_outcomes(passed=1, skipped=1)
