db7a1c6a9d3750323a1f93bdef8be5282f8f7a8c
Christoph Zwerschke <cito@online.de>
2024-07-18 22:18:04 +0200

    feat: Check marker arguments
    docs: Added changelog entry for error for positional asyncio marker arguments.

---
+++ tests/markers/test_invalid_arguments.py
@@ -0,0 +1,85 @@
+from textwrap import dedent
+
+import pytest
+
+
+def test_no_error_when_scope_passed_as_sole_keyword_argument(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import pytest
+
+            @pytest.mark.asyncio(loop_scope="session")
+            async def test_anything():
+                pass
+            """
+        )
+    )
+    result = pytester.runpytest_subprocess()
+    result.assert_outcomes(passed=1)
+    result.stdout.no_fnmatch_line("*ValueError*")
+
+
+def test_error_when_scope_passed_as_positional_argument(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import pytest
+
+            @pytest.mark.asyncio("session")
+            async def test_anything():
+                pass
+            """
+        )
+    )
+    result = pytester.runpytest_subprocess()
+    result.assert_outcomes(errors=1)
+    result.stdout.fnmatch_lines(
+        ["*ValueError: mark.asyncio accepts only a keyword argument*"]
+    )
+
+
+def test_error_when_wrong_keyword_argument_is_passed(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import pytest
+
+            @pytest.mark.asyncio(cope="session")
+            async def test_anything():
+                pass
+            """
+        )
+    )
+    result = pytester.runpytest_subprocess()
+    result.assert_outcomes(errors=1)
+    result.stdout.fnmatch_lines(
+        ["*ValueError: mark.asyncio accepts only a keyword argument 'scope'*"]
+    )
+
+
+def test_error_when_additional_keyword_arguments_are_passed(
+    pytester: pytest.Pytester,
+):
+    pytester.makepyfile(
+        dedent(
+            """\
+            import pytest
+
+            @pytest.mark.asyncio(loop_scope="session", more="stuff")
+            async def test_anything():
+                pass
+            """
+        )
+    )
+    result = pytester.runpytest_subprocess()
+    result.assert_outcomes(errors=1)
+    result.stdout.fnmatch_lines(
+        ["*ValueError: mark.asyncio accepts only a keyword argument*"]
+    )
