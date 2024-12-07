3004bb76050593463819bb809249af76542e54ba
Bryan C. Mills <bryan@duolingo.com>
2024-12-06 23:26:38 -0500

    Simplify contextvars support
    Instead of storing a Context in a context variable, just copy out the
    changes from the setup task's context into the ambient context, and
    reset the changes after running the finalizer task.

--- tests/async_fixtures/test_async_fixtures_contextvars.py
+++ tests/async_fixtures/test_async_fixtures_contextvars.py
@@ -6,31 +6,54 @@ contextvars were not properly maintained among fixtures and tests.
 from __future__ import annotations
 
 import sys
-from contextlib import asynccontextmanager
+from contextlib import contextmanager
 from contextvars import ContextVar
 
 import pytest
 
+_context_var = ContextVar("context_var")
 
-@asynccontextmanager
-async def context_var_manager():
-    context_var = ContextVar("context_var")
-    token = context_var.set("value")
+
+@contextmanager
+def context_var_manager(value):
+    token = _context_var.set(value)
     try:
-        yield context_var
+        yield
     finally:
-        context_var.reset(token)
+        _context_var.reset(token)
 
 
 @pytest.fixture(scope="function")
-async def context_var():
-    async with context_var_manager() as v:
-        yield v
+async def no_var_fixture():
+    with pytest.raises(LookupError):
+        _context_var.get()
+    yield
+    with pytest.raises(LookupError):
+        _context_var.get()
+
+
+@pytest.fixture(scope="function")
+async def var_fixture(no_var_fixture):
+    with context_var_manager("value"):
+        yield
+
+
+@pytest.fixture(scope="function")
+async def var_nop_fixture(var_fixture):
+    with context_var_manager(_context_var.get()):
+        yield
+
+
+@pytest.fixture(scope="function")
+def inner_var_fixture(var_nop_fixture):
+    assert _context_var.get() == "value"
+    with context_var_manager("value2"):
+        yield
 
 
 @pytest.mark.asyncio
 @pytest.mark.xfail(
     sys.version_info < (3, 11), reason="requires asyncio Task context support"
 )
-async def test(context_var):
-    assert context_var.get() == "value"
+async def test(inner_var_fixture):
+    assert _context_var.get() == "value2"
