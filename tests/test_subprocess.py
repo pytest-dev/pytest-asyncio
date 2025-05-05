bb43976777db9f121c07d3d686473ba024413ca6
Michael Seifert <m.seifert@digitalernachschub.de>
2025-05-05 06:39:33 +0200

    refactor: Removed obsolete event_loop definitions in test code.

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -7,16 +7,6 @@ import sys
 
 import pytest
 
-if sys.platform == "win32":
-    # The default asyncio event loop implementation on Windows does not
-    # support subprocesses. Subprocesses are available for Windows if a
-    # ProactorEventLoop is used.
-    @pytest.fixture()
-    def event_loop():
-        loop = asyncio.ProactorEventLoop()
-        yield loop
-        loop.close()
-
 
 @pytest.mark.asyncio
 async def test_subprocess():
