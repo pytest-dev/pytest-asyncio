b3cd305761b17a473ba77d38608ad261aa680464
Michael Seifert <m.seifert@digitalernachschub.de>
2026-06-13 11:13:44 +0200

    typing: Allow AsyncIterable as a return type for async generator fixtures

--- tests/typing.py
+++ tests/typing.py
@@ -2,7 +2,7 @@
 # to allow type checkers to report usage errors.
 # This is necessary, because most other test code is hidden from type checkers
 # through the use of pytest.Pytester
-from collections.abc import AsyncGenerator, AsyncIterator
+from collections.abc import AsyncGenerator, AsyncIterable, AsyncIterator
 
 import pytest_asyncio
 
@@ -19,4 +19,9 @@ async def async_generator0() -> AsyncGenerator:
 
 @pytest_asyncio.fixture
 async def async_generator1() -> AsyncIterator:
+    yield
+
+
+@pytest_asyncio.fixture
+async def async_generator2() -> AsyncIterable:
     yield
